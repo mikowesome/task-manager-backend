5b1327c9a8c3564415311214b706d63f59801805
Miko <jm_mapilisan@yahoo.com>
2022-11-14 23:53:13 +0800

    Update cors origin

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cors({
-    origin: ["http://localhost:3000", ]
+    origin: ["http://localhost:3000", "https://mikowesome-task-manager.netlify.app" ]
 }))
 app.use("/api/v1/tasks", taskRoutes)
 
